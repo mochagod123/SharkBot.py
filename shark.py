9ba7fcf0a437121e4249f59914e8f893b88578f7
暇な人 <yuuta4083@gmail.com>
2025-02-14 14:18:54 +0900

    Add files via upload

--- shark.py
+++ shark.py
@@ -1,117 +1,84 @@
-import aiohttp
-import asyncio
+import requests
 import json
 
 class GetData:
-    async def get_money(guild: int, user: int):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.sharkbot.xyz/api/money?guild={guild}&user={user}") as response:
-                return json.loads(await response.text())
-            
-    async def get_item(user: int):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.sharkbot.xyz/api/item?user={user}") as response:
-                return json.loads(await response.text())
-            
-    async def get_score(guild: int, user: int):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.sharkbot.xyz/api/score?user={user}&guild={guild}") as response:
-                return json.loads(await response.text())
-            
-    async def auth_token(token: str):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.sharkbot.xyz/api/auth?token={token}") as response:
-                return json.loads(await response.text())
-            
-    async def add_money(token: str, user: int, money: int):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.sharkbot.xyz/api/money_add?token={token}&user={user}&money={money}") as response:
-                return json.loads(await response.text())
+    @staticmethod
+    def get_money(guild: int, user: int):
+        response = requests.get(f"https://www.sharkbot.xyz/api/money?guild={guild}&user={user}")
+        return response.json()
+    
+    @staticmethod
+    def get_item(user: int):
+        response = requests.get(f"https://www.sharkbot.xyz/api/item?user={user}")
+        return response.json()
+    
+    @staticmethod
+    def get_score(guild: int, user: int):
+        response = requests.get(f"https://www.sharkbot.xyz/api/score?user={user}&guild={guild}")
+        return response.json()
+    
+    @staticmethod
+    def auth_token(token: str):
+        response = requests.get(f"https://www.sharkbot.xyz/api/auth?token={token}")
+        return response.json()
+    
+    @staticmethod
+    def add_money(token: str, user: int, money: int):
+        response = requests.get(f"https://www.sharkbot.xyz/api/money_add?token={token}&user={user}&money={money}")
+        return response.json()
 
 class Economy:
     def __init__(self, guild: int, user: int):
-        loop = asyncio.get_event_loop()
-        self.data = loop.run_until_complete(GetData.get_money(guild, user))
+        self.data = GetData.get_money(guild, user)
 
     @property
     def coin_name(self):
-        try:
-            return self.data["CoinName"]
-        except:
-            return None
+        return self.data.get("CoinName", None)
     
     @property
     def money(self):
-        try:
-            return self.data["Money"]
-        except:
-            return None
+        return self.data.get("Money", None)
         
 class AddEconomyMoney:
     def __init__(self, token: str, user: int, money: int):
-        loop = asyncio.get_event_loop()
-        self.data = loop.run_until_complete(GetData.add_money(token, user, money))
+        self.data = GetData.add_money(token, user, money)
 
 class EconomyItems:
     def __init__(self, user: int):
-        loop = asyncio.get_event_loop()
-        self.data = loop.run_until_complete(GetData.get_item(user))
+        self.data = GetData.get_item(user)
 
     @property
     def item(self):
-        try:
-            return self.data["Items"]
-        except:
-            return None
+        return self.data.get("Items", None)
         
 class Score:
     def __init__(self, guild: int, user: int):
-        loop = asyncio.get_event_loop()
-        self.data = loop.run_until_complete(GetData.get_score(guild, user))
+        self.data = GetData.get_score(guild, user)
 
     @property
     def score(self):
-        try:
-            return self.data["Score"]
-        except:
-            return None
+        return self.data.get("Score", None)
         
 class Auth:
     def __init__(self, token: str):
-        loop = asyncio.get_event_loop()
-        self.data = loop.run_until_complete(GetData.auth_token(token))
+        self.data = GetData.auth_token(token)
 
     @property
     def guild_id(self):
-        try:
-            return self.data["Guild"]
-        except:
-            return None
-        
+        return self.data.get("Guild", None)
+    
     @property
     def guild_name(self):
-        try:
-            return self.data["GuildName"]
-        except:
-            return None
-        
+        return self.data.get("GuildName", None)
+    
     @property
     def author_id(self):
-        try:
-            return self.data["Author"]
-        except:
-            return None
-        
+        return self.data.get("Author", None)
+    
     @property
     def author_name(self):
-        try:
-            return self.data["AuthorName"]
-        except:
-            return None
+        return self.data.get("AuthorName", None)
     
     @property
     def token(self):
-        try:
-            return self.data["Token"]
-        except:
-            return None
+        return self.data.get("Token", None)

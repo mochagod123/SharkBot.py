32dfa9f7869c6c6f3356d9c3cde079cbf3760bb1
暇な人 <yuuta4083@gmail.com>
2025-03-12 13:10:53 +0900

    最新版に更新

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import discord
-from discord.ext import commands
 import shark
-import asyncio
-from functools import partial
 
-bot = commands.Bot(command_prefix="shark!", intents=discord.Intents.all(), help_command=None)
+auth = shark.Auth("")
 
-@bot.command(name="money")
-async def money(ctx: commands.Context):
-    loop = asyncio.get_event_loop()
-    moneys = await loop.run_in_executor(None, partial(shark.Economy, ctx.guild.id, ctx.author.id))
-    await ctx.reply(f"お金: {moneys.money}{moneys.coin_name}")
-
-bot.run("Token")
+print(auth.token)
+print(auth.guild_name)
+print(auth.guild_id)
+print(auth.author_name)
+print(auth.author_id)
--- shark.py
+++ shark.py
@@ -1,67 +1,18 @@
 import requests
 import json
 
+base_url = "https://api.sharkbot.xyz"
+
 class GetData:
-    @staticmethod
-    def get_money(guild: int, user: int):
-        response = requests.get(f"https://www.sharkbot.xyz/api/money?guild={guild}&user={user}")
-        return response.json()
-    
-    @staticmethod
-    def get_item(user: int):
-        response = requests.get(f"https://www.sharkbot.xyz/api/item?user={user}")
-        return response.json()
-    
-    @staticmethod
-    def get_score(guild: int, user: int):
-        response = requests.get(f"https://www.sharkbot.xyz/api/score?user={user}&guild={guild}")
-        return response.json()
-    
+
     @staticmethod
     def auth_token(token: str):
-        response = requests.get(f"https://www.sharkbot.xyz/api/auth?token={token}")
-        return response.json()
-    
-    @staticmethod
-    def add_money(token: str, user: int, money: int):
-        response = requests.get(f"https://www.sharkbot.xyz/api/money_add?token={token}&user={user}&money={money}")
+        response = requests.post(base_url + "/auth", json={"token": token})
         return response.json()
 
-class Economy:
-    def __init__(self, guild: int, user: int):
-        self.data = GetData.get_money(guild, user)
-
-    @property
-    def coin_name(self):
-        return self.data.get("CoinName", None)
-    
-    @property
-    def money(self):
-        return self.data.get("Money", None)
-        
-class AddEconomyMoney:
-    def __init__(self, token: str, user: int, money: int):
-        self.data = GetData.add_money(token, user, money)
-
-class EconomyItems:
-    def __init__(self, user: int):
-        self.data = GetData.get_item(user)
-
-    @property
-    def item(self):
-        return self.data.get("Items", None)
-        
-class Score:
-    def __init__(self, guild: int, user: int):
-        self.data = GetData.get_score(guild, user)
-
-    @property
-    def score(self):
-        return self.data.get("Score", None)
-        
 class Auth:
     def __init__(self, token: str):
-        self.data = GetData.auth_token(token)
+        self.data = GetData().auth_token(token)
 
     @property
     def guild_id(self):
